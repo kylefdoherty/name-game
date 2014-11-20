b54d53eae2c30cbedfbd55223ffa65b4b78d3f4e
kylefdoherty <kyledoherty22@gmail.com>
2014-11-19 20:07:30 -0500

    Removed comments from test

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
-feature "Playing a new game" do 
+feature "Play a new game (create new game)" do 
   scenario "Visit homepage and click 'Play New Game'" do 
     visit '/'
     click_link "Play New Game"
 
     expect(page).to have_content "Game 1"
     expect(page.current_url).to eq("http://www.example.com/games")
-    # expect(page).to have checkboxes NYC, Lon, HK, Dubai, All
-    # expect(page).to have content "Start Game"
   end 
 end 
