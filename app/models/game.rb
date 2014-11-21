bfb3314b8e27455881a8ff0a3e93221415f2cff2
Kyle Doherty <kyledoherty22@gmail.com>
2014-11-21 17:49:54 -0500

    Made so game model creates question groups after its created

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,17 +1,27 @@
 class Game < ActiveRecord::Base
+  after_save :game_setup
   has_many :question_groups
   has_many :questions, through: :question_groups
 
   def game_setup
     teammates = Teammate.where(office: office)
 
-    teammates.each do |t|
-      questions = [Question.new(text: t.name), Question.new(text: t.title), Question.new(text: t.team)]
+    teammates.each do |teammate|
+      questions = question_group_questions(teammate)
 
-
-      question_groups << QuestionGroup.create(image: t.image, questions: questions)
+      question_groups << QuestionGroup.create(image: teammate.image, questions: questions)
     end
   end
+
+  private
+
+    def question_group_questions(teammate)
+      [
+        Question.new(text: teammate.name, label: "name"),
+        Question.new(text: teammate.title, label: "title"),
+        Question.new(text: teammate.team, label: "team")
+      ]
+    end
 
 end
 
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 describe Game do
-  #build 3 teammates with 3 different offices
-  #make these factories later
   let!(:teammates) {
     [
-      create(:teammate, name: "Bruce Wayne", title: "Batman", office: "NYC", team: "Merica", image: "images/batman.jpg"),
+      create(:teammate,
+             name: "Bruce Wayne",
+             title: "Batman",
+             office: "NYC",
+             team: "Merica",
+             image: "images/batman.jpg"),
       create(:teammate, office: "HK")
     ]
   }
@@ -14,28 +17,22 @@ describe Game do
 
   describe "#game_setup" do
     it "builds the question_groups" do
-      game.game_setup
-
       expect(game.question_groups.length).to eq(1)
     end
 
     it "builds the questions for each question_group" do
-      game.game_setup
-
       questions = game.question_groups.first.questions
       expect(questions.length).to eq(3)
     end
 
     it "builds the correct questions for each question group" do
-      game.game_setup
+      name = game.question_groups.first.questions.first.text
+      title = game.question_groups.first.questions.second.text
+      team = game.question_groups.first.questions.third.text
 
-      name_quesiton = game.question_groups.first.questions.first.text
-      title_question = game.question_groups.first.questions.second.text
-      team_question = game.question_groups.first.questions.third.text
-
-      expect(name_quesiton).to eq("Bruce Wayne")
-      expect(title_question).to eq("Batman")
-      expect(team_question).to eq("Merica")
+      expect(name).to eq("Bruce Wayne")
+      expect(title).to eq("Batman")
+      expect(team).to eq("Merica")
     end
   end
 end
