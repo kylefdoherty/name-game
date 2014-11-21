24bbe6025defdeea263424712042b150bc56fe61
Kyle Doherty <kyledoherty22@gmail.com>
2014-11-21 17:47:39 -0500

    Create question_group_controller

---
+++ app/controllers/question_groups_controller.rb
@@ -0,0 +1,34 @@
+class QuestionGroupsController < ApplicationController
+  before_action :set_question_group, only: [:show, :update]
+
+  def show
+    @game = Game.find(params[:game_id])
+    @question1, @question2, @question3 = @question_group.questions
+  end
+
+  def update
+    question_ids = params[:question_group][:questions]
+
+    question_ids.each do |id|
+      if id.present?
+        Question.find(id).update(correct: true)
+      end
+    end
+
+
+    game = Game.find(@question_group.game_id)
+
+    if @question_group.id == game.question_groups.last.id
+      redirect_to root_path
+    else
+      redirect_to game_question_group_path(game, QuestionGroup.find(@question_group.id + 1))
+    end
+  end
+
+  private
+
+    def set_question_group
+      @question_group = QuestionGroup.find(params[:id])
+    end
+end
+
